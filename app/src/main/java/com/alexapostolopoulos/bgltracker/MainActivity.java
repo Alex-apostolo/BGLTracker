e2a5f7e9a4f9b662e98d4bdfd22568b187716c31
Alex-apostolo <alex-apostolo@hotmail.com>
2019-03-10 16:17:54 +0000

    Initial commit

---
+++ app/src/main/java/com/alexapostolopoulos/bgltracker/MainActivity.java
@@ -0,0 +1,47 @@
+package com.alexapostolopoulos.bgltracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomRowData firstRow = new CustomRowData("Insulin", "5670 mmlg", LocalDate.of(2018,12,20), LocalTime.of(20,20));
+        CustomRowData secondRow = new CustomRowData("Glucose", "lol mmlg", LocalDate.of(2018,12,10), LocalTime.of(20,20));
+        CustomRowData[] myData = {firstRow,secondRow};
+        ListAdapter adapter = new CustomAdapter(this, myData);
+        ListView BGLlist = findViewById(R.id.BGLlist);
+        BGLlist.setAdapter(adapter);
+        BGLlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    public void createAddInsulinActivity(View v) {
+        Intent intent = new Intent(this, AddInsulinActivity.class);
+        startActivity(intent);
+    }
+}
