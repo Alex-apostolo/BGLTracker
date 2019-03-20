f514de8405f33fd214fc4e91ceeecf2fa8461e23
Alex-apostolo <alex-apostolo@hotmail.com>
2019-03-20 15:15:05 +0000

    Added Medication Activity

--- app/src/main/java/com/alexapostolopoulos/bgltracker/AddGlucoseActivity.java
+++ app/src/main/java/com/alexapostolopoulos/bgltracker/AddGlucoseActivity.java
@@ -3,12 +3,6 @@ package com.alexapostolopoulos.bgltracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class AddGlucoseActivity extends AppCompatActivity {
 
@@ -16,10 +10,6 @@ public class AddGlucoseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_glucose);
-        EditText date = findViewById(R.id.date_inputField);
-        EditText time = findViewById(R.id.time_inputField);
-        date.setText(LocalDate.now().toString());
-        time.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
