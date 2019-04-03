c95fa730262e418c70ddfd79418df6495732dbb1
Alex-apostolo <alex-apostolo@hotmail.com>
2019-04-03 17:05:35 +0100

    Fixed minor bug

--- app/src/main/java/com/alexapostolopoulos/bgltracker/AddPrescriptionActivity.java
+++ app/src/main/java/com/alexapostolopoulos/bgltracker/AddPrescriptionActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class AddPrescriptionActivity extends AppCompatActivity {
 
