8f4409d5a6004494054e54251c7d09d0194b4caf
Alex-apostolo <alex-apostolo@hotmail.com>
2019-04-06 16:22:59 +0100

    Merged Xanders version with main one

--- app/src/main/java/com/alexapostolopoulos/bgltracker/AddCustomInsulinActivity.java
+++ app/src/main/java/com/alexapostolopoulos/bgltracker/AddCustomInsulinActivity.java
@@ -1,7 +1,7 @@
 package com.alexapostolopoulos.bgltracker;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
