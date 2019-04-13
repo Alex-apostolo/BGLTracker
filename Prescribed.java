abffc48cf25a6308ffe6f0a2a97e17b774082afe
Greenslycer <xanwelsh@Hotmail.co.uk>
2019-04-13 14:47:46 +0100

    Delete Prescribed.java

--- Prescribed.java
+++
@@ -1,7 +0,0 @@
-package com.alexapostolopoulos.bgltracker;
-
-import com.alexapostolopoulos.bgltracker.Model.Prescription;
-
-public interface Prescribed {
-    void editPrescription(Prescription p);
-}
